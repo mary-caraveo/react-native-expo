ff54da70c10843b98e78a6e9d1535f51fd278a25
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-11-04 15:32:30 -0500

    chore: remove styles

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navigation/NavigationStack';
-import  NavigationTab from './src/navigation/NavigationTab';
-import  NavigationDrawer from './src/navigation/NavigationDrawer';
+import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       {/* <NavigationStack /> */}
@@ -16,11 +15,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
